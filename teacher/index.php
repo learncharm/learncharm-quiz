f7c9e56d9a19d09403afc4328059b6c5f7ddc545
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-03-18 16:38:33 +0530

    teacher dashboard sidebar

---
+++ teacher/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher | Learncharm Quiz</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- font awesone -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- flowbite -->
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.3.4/dist/flowbite.min.css" />
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/favicon-16x16.png">
+<link rel="manifest" href="../images/favicon/site.webmanifest">
+</head>
+<body>
+    <?php
+        include './sidebar.php';
+    ?>
+
+<!-- javasript -->
+    <!-- flowbite -->
+    <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
+</body>
+</html>
---
+++ teacher/sidebar.php
@@ -0,0 +1,37 @@
+<div class="flex felx-col">
+        <div class="fixed top-0 left-0 h-screen w-16 m-0 inline-block align-top bg-gray-900 text-white shadow-lg">
+                        <ul>
+                            <!-- logo -->
+                            <li><a href="../" data-tooltip-target="tooltip-logo" data-tooltip-placement="right" ><img src="../images/logo/logo.png" class="fas fa-plus relative flex items-center justify-center h-12 w-12 mt-2 mb-2 p-2 mx-auto bg-gray-700 text-green-400 transition-all duration-200 ease-linear cursor-pointer rounded-3xl hover:bg-green-400 hover:text-white hover:rounded-xl" ></img></a></li>
+                            <span id="tooltip-logo" role="tooltip" class="inline-block absolute invisible z-40 py-2 px-3 m-4 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"  >Go to Home
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                            </span>
+                            <hr class="w-12 relative left-2 border-green-400">
+
+                            <!-- icon 1 -->
+                            <li><a href="#" data-tooltip-target="tooltip-right" data-tooltip-placement="right" ><i class="fas fa-plus relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-700 text-green-400 transition-all duration-200 ease-linear cursor-pointer rounded-3xl hover:bg-green-400 hover:text-white hover:rounded-xl" id="icon1"></i></a></li>
+                            <span id="tooltip-right" role="tooltip" class="inline-block absolute invisible py-2 px-3 z-10 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700" >Create Quiz
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                            </span>
+                            <!-- icon 2 -->
+                            <li><a href="#" data-tooltip-target="tooltip-all-quiz" data-tooltip-placement="right" ><i class="fas fa-chalkboard-user relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-700 text-green-400 transition-all duration-200 ease-linear cursor-pointer rounded-3xl hover:bg-green-400 hover:text-white hover:rounded-xl" id="icon1"></i></a></li>
+                            <span id="tooltip-all-quiz" role="tooltip" class="inline-block absolute invisible py-2 px-3 z-10 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700" >All Quiz
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                            </span>
+                            <hr class="w-12 relative left-2 border-green-400">
+
+                            <!-- icon 3-->
+                            <li><a href="#" data-tooltip-target="tooltip-create-group" data-tooltip-placement="right" ><i class="fas fa-folder-plus relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-700 text-green-400 transition-all duration-200 ease-linear cursor-pointer rounded-3xl hover:bg-green-400 hover:text-white hover:rounded-xl" id="icon1"></i></a></li>
+                            <span id="tooltip-create-group" role="tooltip" class="inline-block absolute invisible py-2 px-3 z-10 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700" >Create Group
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                            </span>
+
+                            <!-- icon 3-->
+                            <li><a href="#" data-tooltip-target="tooltip-group" data-tooltip-placement="right" ><i class="fa-solid fa-folder relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-700 text-green-400 transition-all duration-200 ease-linear cursor-pointer rounded-3xl hover:bg-green-400 hover:text-white hover:rounded-xl" id="icon1"></i></a></li>
+                            <span id="tooltip-group" role="tooltip" class="inline-block absolute invisible py-2 px-3 z-10 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700" >5th Class
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                            </span>
+                        </ul>
+                        
+            </div>
+    </div>
