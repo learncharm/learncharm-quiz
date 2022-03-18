2b3ca463e145c357de39887279ba35011ef4d4f0
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-03-18 16:13:00 +0530

    menu

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learncharm Quiz</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <!-- header -->
+    <div class="flex  bg-green-300 p-4">
+        <h1 class="text-xl text-green-800 font-bold">Learncharm Quiz</h1>
+        <ul class="flex relative ml-96 left-96 bottom-2">
+            <li class="m-2"><a href="#" class="text-xl font-semibold">Home</a></li>
+            <li class="m-2"><a href="./teacher/" class="text-xl font-semibold">For Teachers</a></li>
+            <li class="m-2"><a href="#" class="text-xl font-semibold">For Students</a></li>
+        </ul>
+    </div>
+</body>
+</html>
